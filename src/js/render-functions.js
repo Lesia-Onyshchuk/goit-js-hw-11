325d2d53f73ed3735010b4a358e7609b3f39b339
lesia_onyshchuk <lkane443@gmail.com>
2024-12-04 01:00:35 +0200

    css

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -14,26 +14,24 @@ export default function markup(arr) {
              <a class="gallery-link" href="${largeImageURL}">
                <img class="gallery-img" src="${webformatURL}" alt="${tags}" width="300">
              </a>
-             <div>
                <ul class="image-description">
                <li>
-                 <h2>Likes</h2>
+                 <h2 class="title">Likes</h2>
                  <p>${likes}</p>
                </li>
                <li>
-               <h2>Views</h2>
+               <h2 class="title">Views</h2>
                <p>${views}</p>
                </li>
                <li>
-                 <h2>Comments</h2>
+                 <h2 class="title">Comments</h2>
                  <p>${comments}</p>
                </li>
                <li>
-                 <h2>Downloads</h2>
+                 <h2 class="title">Downloads</h2>
                  <p>${downloads}</p>
                </li>
                </ul>
-             </div>  
        </li>`
     )
     .join('');
